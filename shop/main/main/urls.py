479a2f4f9d09d32747093b8809d8bc293caee8e0
Chimcuxanh <bestlucian16@gmail.com>
2021-09-19 10:29:37 +0700

    complete

--- shop/main/main/urls.py
+++ shop/main/main/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/',include('shopping.urls')),
 ]
 
